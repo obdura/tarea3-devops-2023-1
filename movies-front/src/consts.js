ee9a5179c748264797cc90f54cd06dff096617c6
Sebastian Gonzalez Maturana <sigonzam@gmail.com>
2023-08-07 20:05:41 -0400

    fix: change react apiServer

--- movies-front/src/consts.js
+++ movies-front/src/consts.js
@@ -1,3 +1,4 @@
-const apiServer = `${process.env.REACT_APP_API_URI}`
+//const apiServer = `${process.env.REACT_APP_API_URI}`
+const apiServer = `http://localhost:${process.env.REACT_APP_API_PORT}`
 
 export default apiServer
